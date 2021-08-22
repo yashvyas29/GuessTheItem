8063bf445f3391a69fef9a18eb862c00559336e7
Yash Vyas <vyasyas@hilti.com>
2021-08-22 21:57:11 +0530

    Initial Commit

---
+++ Shared/GuessTheItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheItemApp.swift
+//  Shared
+//
+//  Created by global on 22/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
